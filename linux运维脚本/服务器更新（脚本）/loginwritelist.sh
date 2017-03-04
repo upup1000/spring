899086891fe12a0ce537c0010fd2f0dda4da6126
upup1000 <zssgood@126.com>
2017-03-04 18:39:20 +0800

    delete

--- linux运维脚本/服务器更新（脚本）/loginwritelist.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-dbip=$1
-###1开启0关闭###
-isopen=$2
-###如果开启时给玩家的提示
-tips='服务器处于维护期间，预计11点开服，尽情期待'
-mysql --default-character-set=utf8 -usaintdbcon -p87ohB07jG7 -h${dbip} -e "update heeking_gamedata.game_param set v=${isopen} where id=68"
-mysql --default-character-set=utf8 -usaintdbcon -p87ohB07jG7 -h${dbip} -e "update heeking_gamedata.game_param set v='${tips}' where id=69"
-mysql --default-character-set=utf8 -usaintdbcon -p87ohB07jG7 -h${dbip} -e "update heeking_gamedata.dbcachelist set version=version+1 where cachename='game_param'"
--- linux运维脚本/服务器更新（脚本）/monitorTomcat2.sh
+++
@@ -1,2 +0,0 @@
-DATE=$(date +%Y-%m-%d)
-grep 'Exception:' -A 20 /data/heeking/apache-tomcat-8.0.26/logs/catalina.out|grep 2016-05-06 -A 20 > /usr/tomcat/logs/outMonitorLog.log
